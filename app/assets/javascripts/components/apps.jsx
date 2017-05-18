c1d81c43fdb49fd42af0bdfb949faeb8c903d08a
muhamaddiaz <muhamaddiaz10@gmail.com>
2017-05-18 08:01:36 +0700

    new initialize

---
+++ app/assets/javascripts/components/apps.jsx
@@ -0,0 +1,14 @@
+// app/assets/javascripts/components/post.jsx
+
+window.Post = React.createClass({
+  render: function() {
+    return <h1>{this.props.title}</h1>
+  }
+})
+
+// or, equivalent:
+class Post extends React.Component {
+  render() {
+    return <h1>{this.props.title}</h1>    
+  }
+}
